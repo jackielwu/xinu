6a6abb460db9d64f1e6ccc32c73f89738d14093a
Jackie Wu <jackielwu@gmail.com>
2018-02-10 18:45:20 -0500

    lab2- 3.1 tested pass

--- system/test.c
+++ system/test.c
@@ -1,9 +1,11 @@
 #include <xinu.h>
 //#include "../system/host2netca.c"
+extern long host2net(long x);
 extern long host2netca(long x);
 
 int main()
 {
   long num = 69;
+  kprintf("%d\n", host2net(num));
   kprintf("%d\n", host2netca(num));
 }
