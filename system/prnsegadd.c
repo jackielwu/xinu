6714bf0d5d7a1cec37a609fb2d73957b4304d2c9
Jackie Wu <jackielwu@gmail.com>
2018-02-13 16:58:08 -0500

    fixed xinu end

--- system/prnsegadd.c
+++ system/prnsegadd.c
@@ -5,7 +5,7 @@ void prnsegadd()
   kprintf("Start of Text at 0x%08X, end of Text at 0x%08X.\n", (uint32)&text, (uint32)&etext-1);
   kprintf("Start of Data at 0x%08X, end of Data at 0x%08X.\n", (uint32)&data, (uint32)&edata-1);
   kprintf("Start of Bss at 0x%08X, end of Bss at 0x%08X.\n", (uint32)&bss, (uint32)&ebss-1);
-  kprintf("End of XINU at 0x%08X.\n", (uint32)&text-1);
+  kprintf("End of XINU at 0x%08X.\n", (uint32)&text);
   kprintf("Text Length : %d bytes\n", (uint32)&etext - (uint32)&text);
   kprintf("Data Length : %d bytes\n", (uint32)&edata - (uint32)&data);
   kprintf("BSS Length : %u bytes\n", (uint32)&ebss - (uint32)&bss);
